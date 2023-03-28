fd4b40d85cfb7eefb109c3bcd6f541d6366c590a
Dan Cornilov <dancornilov@Dans-MacBook-Pro.local>
2023-03-28 23:37:24 +0300

    feat: install material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxToastModule } from "@angular-magic/ngx-toast";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NgxToastModule } from "@angular-magic/ngx-toast";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxToastModule
+    NgxToastModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
